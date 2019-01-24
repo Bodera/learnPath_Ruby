ac9feb617ec434a73bc4b52cb339650725bec55b
Bodera <rafaelrafa990@gmail.com>
2019-01-24 11:52:49 -0200

    Modelagem do problema 1010.

---
+++ URI/débutant/ex1010.rb
@@ -0,0 +1,32 @@
+=begin
+S I M P L E   C A L C U L A T E
+In this problem, the task is to read a code of a product 1, the number of units of product 1, the price for one unit of product 1, the code of a product 2, the number of units of product 2 and the price for one unit of product 2. After this, calculate and show the amount to be paid.
+
+Input:
+The input file contains two lines of data. In each line there will be 3 values: two integers and a floating value with 2 digits after the decimal point.
+1 1 15.10
+2 1 15.10
+
+Output:
+The output file must be a message like the following example where "Valor a pagar" means Value to Pay. Remember the space after ":" and after "R$" symbol. The value must be presented with 2 digits after the point.
+VALOR A PAGAR: R$ 30.20
+
+https://udebug.com/URI/1010
+=end
+#cria função que calcula e retorna o resultado.
+    def simpleCalculate()
+        #multiplica o valor pelas unidades da linha 1.
+        
+        #multiplica o valor pelas unidades da linha 2.
+        
+        #soma os valores.
+        
+        #apresenta o resultado.
+    end
+
+#Codigo, unidades e valor da linha 1.
+
+#Código, unidades e valor da linha 2.
+
+#chama a função passando os parâmetros.
+simpleCalculate()
