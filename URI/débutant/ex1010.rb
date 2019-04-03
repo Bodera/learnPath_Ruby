c9747c244403c3cbc41830771aab87ca530649d4
Bode <rafaelrafa990@gmail.com>
2019-04-03 10:52:12 -0300

    solution 1010 approuvée

--- URI/débutant/ex1010.rb
+++ URI/débutant/ex1010.rb
@@ -1,9 +1,12 @@
 =begin
 S I M P L E   C A L C U L A T E
-In this problem, the task is to read a code of a product 1, the number of units of product 1, the price for one unit of product 1, the code of a product 2, the number of units of product 2 and the price for one unit of product 2. After this, calculate and show the amount to be paid.
+In this problem, the task is to read a code of a product 1, the number of units of product 1, the price for one unit of product 1,
+the code of a product 2, the number of units of product 2 and the price for one unit of product 2. 
+After this, calculate and show the amount to be paid.
 
 Input:
-The input file contains two lines of data. In each line there will be 3 values: two integers and a floating value with 2 digits after the decimal point.
+The input file contains two lines of data. In each line there will be 3 values: two integers and a floating value with 2 digits 
+after the decimal point.
 1 1 15.10
 2 1 15.10
 
@@ -13,20 +16,11 @@ VALOR A PAGAR: R$ 30.20
 
 https://udebug.com/URI/1010
 =end
-#cria função que calcula e retorna o resultado.
-    def simpleCalculate()
-        #multiplica o valor pelas unidades da linha 1.
-        
-        #multiplica o valor pelas unidades da linha 2.
-        
-        #soma os valores.
-        
-        #apresenta o resultado.
-    end
+ligne1 = gets.chomp
+code1, unites1, prix1 = ligne1.split(" ")
 
-#Codigo, unidades e valor da linha 1.
+ligne2 = gets.chomp
+code2, unites2, prix2 = ligne2.split(" ")
 
-#Código, unidades e valor da linha 2.
-
-#chama a função passando os parâmetros.
-simpleCalculate()
+resultat = ((unites1.to_i*prix1.to_f)+(unites2.to_i*prix2.to_f))
+print "VALOR A PAGAR: R$ #{'%.2f' % resultat}\n"
